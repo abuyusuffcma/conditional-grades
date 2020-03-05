cb4951bead85094f4f6cddcf591040ad16b54403
Md. Abu Yusuf <abuyusuffcma@gmail.com>
2020-03-05 20:50:20 +0600

    function

---
+++ function.js
@@ -0,0 +1,18 @@
+function doubleIt(num){
+    var result = num * 2;
+    return result;
+}
+
+var first = doubleIt(5);
+
+var second = doubleIt(100);
+var total = first+second;
+console.log(total);
+
+function add(num1, num2){
+ var result = num1+num2;
+ return result;
+}
+
+var result = add(15, 18);
+console.log(result);
