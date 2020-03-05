04564c5965067636e63dcdbb0dd4dd1054ffe122
Md. Abu Yusuf <abuyusuffcma@gmail.com>
2020-03-05 19:53:09 +0600

    for loop

---
+++ forloop.js
@@ -0,0 +1,11 @@
+/*var nums = [55, 66, 77, 88, 99, 11, 33, 22];
+for(var i = 0; i<nums.length; i++){
+    var element = nums[i];
+    console.log(element);
+}*/
+
+var friendsName = ["Mukbul", "Manik", "Iqbal", "Jamal", "Riaz"];
+for( var i = 0; i<friendsName.length; i++){
+    var element = friendsName[i];
+    console.log(element)
+}
---
+++ while.js
@@ -0,0 +1,5 @@
+var num = 8; 
+while(num<15){
+    console.log(num);
+    num++;
+}
