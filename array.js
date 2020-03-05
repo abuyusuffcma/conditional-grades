d9a9f115850117941b833cdcb0762a48c1202018
Md. Abu Yusuf <abuyusuffcma@gmail.com>
2020-03-05 18:51:14 +0600

    array

---
+++ array.js
@@ -0,0 +1,12 @@
+var friendsAge = [12, 25, 36, 24, 21, 27];
+//console.log(friendsAge[3]);
+
+//var anisAge = friendsAge[5];
+//console.log(anisAge);
+
+
+//friendsAge[0]= 36;
+//console.log(friendsAge);
+
+var position = friendsAge.indexOf(36);
+console.log(position);
