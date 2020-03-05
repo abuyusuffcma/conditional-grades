9db5752e9ad05f8c2dd3f4e71b0f7312f682189a
Md. Abu Yusuf <abuyusuffcma@gmail.com>
2020-03-05 19:14:14 +0600

    js-array

--- array.js
+++ array.js
@@ -8,5 +8,15 @@ var friendsAge = [12, 25, 36, 24, 21, 27];
 //friendsAge[0]= 36;
 //console.log(friendsAge);
 
-var position = friendsAge.indexOf(36);
-console.log(position);
+//var position = friendsAge.indexOf(36);
+//console.log(position);
+friendsAge.push(29);
+console.log(friendsAge);
+console.log(friendsAge.length);
+friendsAge.pop();
+console.log(friendsAge);
+friendsAge.unshift(192, 254, 300);
+console.log(friendsAge);
+friendsAge.shift();
+console.log(friendsAge);
+
